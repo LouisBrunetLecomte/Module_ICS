c68bca6c31babc6e41a71b91507b8c36b655bc4b
Louis Brunet-Lecomte <louis.brunetlecomte@eleve.leschartreux.net>
2026-03-24 21:23:38 +0100

    push fin séance

--- Module_JavaPoo/TP3/Exercice2/Exo2/src/Article.java
+++ Module_JavaPoo/TP3/Exercice2/Exo2/src/Article.java
@@ -1,9 +1,9 @@
 public class Article {
-    int numero;
-    String reference;
-    String designation;
-    double prix;
-    int nbarticle;
+    private static int numero;
+    private String reference;
+    private String designation;
+    private double prix;
+    private int nbarticle;
 
     // Constructeur
     public Article(){
@@ -16,13 +16,35 @@ public class Article {
 
     }
     public Article(String reference, String designation, double prix){
-        this.numero = numero;
+        numero += 1;
         this.reference = reference;
         this.designation = designation;
+        this.prix = prix;
     }
 
     public String toString(){
-        return "test";
+        return "Numéro de la bd : " + this.numero + " | référence : " + this.reference + " | Titre" + this.designation + " | Prix : " + this.prix + "€";
     }
 
+    // Getter
+    public String getReferance(){
+        return this.reference;
+    }
+    public String getDesignation(){
+        return this.designation;
+    }
+    public double getPrix(){
+        return this.prix;
+    }
+
+    // setter
+    public void setReference(String NewReference){
+        this.reference = NewReference;
+    }
+    public void setDesignation(String NewDesignation){
+        this.designation = NewDesignation;
+    }
+    public void setPrix(double NewPrix){
+        this.prix = NewPrix;
+    }
 }
--- Module_JavaPoo/TP3/Exercice2/Exo2/src/ProgPrincipal.java
+++ Module_JavaPoo/TP3/Exercice2/Exo2/src/ProgPrincipal.java
@@ -1,6 +1,9 @@
 public class ProgPrincipal {
-    public static void test(String[] args) { 
+    public static void main(String[] args) { 
         Article MonArticle = new Article("TINTINN01", "Tintin au congo", 13.50);
         System.out.println(MonArticle); 
+
+        Article MonArticle2 = new Article("TINTINN02", "Le crabe aux pinces d'or", 15.50);
+        System.out.println(MonArticle2); 
     }
 }
